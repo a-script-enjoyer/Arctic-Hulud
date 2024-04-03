2641873d0f4406cca320ccbcf869604a451ae503
a-script-enjoyer <andrey.filipov@gmail.com>
2024-04-03 11:34:16 +0300

    cleaned up bugs detected automatically by debugger

--- menus/start.gd
+++ menus/start.gd
@@ -11,24 +11,29 @@ const gameplay_scene: PackedScene = preload("res://gameplay/gameplay.tscn")
 @onready var scream: AudioStreamPlayer = %Scream
 @onready var timer = %Timer
 
+var skip_request: bool = false
+var start_pressed: bool = false
 var speed: int = 100
 var step: int = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var h_score: int = Global.SAVE_DATA.high_score
 	high_score.text = "High Score: " + str(h_score)
-	if _skip_scream:
-		return
 	thumper_animation.play("thumper_hit")
 	
 func _on_start_pressed():
 	scream_played.emit()
-	scream_played.connect(_skip_scream)
-	scream.play()
-	start.text = "Skip"
+	if not start_pressed:
+		scream_played.connect(_skip_scream)
+	if not skip_request:
+		start_pressed = true
+		scream.play()
+		start.text = "Skip"
 
 func _on_quit_pressed():
 	get_tree().quit()
 	
 func _skip_scream():
+	skip_request = true
+	scream.stop()
 	get_tree().change_scene_to_packed(gameplay_scene)
